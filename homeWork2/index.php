6a24afc3c32ec5b0689302f4160ce7a393906a94
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2018-12-30 16:29:32 +0200

    add hw

---
+++ homeWork2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>HomeWork2</title>
+</head>
+<body>
+	
+	<?php
+		$items = [1, 5, 6, 2, 5, 8];
+		sort($items);
+		var_dump($items);
+	?>
+
+
+</body>
+</html>
