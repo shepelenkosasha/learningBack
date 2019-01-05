b78cfd7e5ca04b987711b9024fa8fdc70ca676b9
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-01-05 15:10:24 +0200

    add

--- arrays_loops_tasks/27.php
+++ arrays_loops_tasks/27.php
@@ -41,6 +41,35 @@
 	</tr>
 </table>*/
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Table rand</title>
+</head>
+<body>
+
+	<?php 
+
+		$rows = rand(2, 10);
+		$cols = rand(2, 10);
+		$colors = ['red','yellow','blue','gray','maroon','brown','green'];
+
+		echo '<table border = "5">';
+		for ($i = 0; $i < $rows; $i++) {
+		    echo '<tr>';
+		    for ($j = 0; $j < $cols; $j++) {
+		        $color = $colors[array_rand($colors)];
+		        $number = rand();
+		        echo "<td style='background-color:{$color}'>{$number}</td>";
+		    }
+		    echo '</tr>';
+		}
+		echo '</table>';
+
+	?>
+	
+</body>
+</html>
